3947b509565b1c665bb9a682e5b3b3c66e328d6f
Yoshiki Shin'ya <785431ys@gmail.com>
2023-05-24 13:02:11 +0900

    Java SpringでHello Worldを実施

---
+++ src/main/java/com/example/hellowrld/HellowrldApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellowrld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowrldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowrldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hellowrld/HellowrldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellowrld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowrldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
